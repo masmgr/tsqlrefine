50110974265194772e522a0a409406a37941901b
Masaki Ikeda <imasaki20@gmail.com>
2026-07-18 22:49:04 +0900

    refactor: share GROUP BY column analysis

---
+++ src/TsqlRefine.Rules/Helpers/Analysis/GroupByColumnAnalysisHelpers.cs
@@ -0,0 +1,367 @@
+using System.Text;
+using Microsoft.SqlServer.TransactSql.ScriptDom;
+
+namespace TsqlRefine.Rules.Helpers.Analysis;
+
+/// <summary>
+/// Provides shared column and expression analysis for GROUP BY validation rules.
+/// </summary>
+public static class GroupByColumnAnalysisHelpers
+{
+    public static List<ScalarExpression> CollectGroupByExpressions(GroupByClause groupBy)
+    {
+        ArgumentNullException.ThrowIfNull(groupBy);
+        var expressions = new List<ScalarExpression>();
+
+        foreach (var specification in groupBy.GroupingSpecifications)
+        {
+            CollectGroupByExpressionsFromSpecification(specification, expressions);
+        }
+
+        return expressions;
+    }
+
+    public static void CollectColumnReferences(
+        ScalarExpression? expression,
+        List<ColumnReferenceExpression> result,
+        List<ScalarExpression> groupByExpressions)
+    {
+        if (expression is not null && IsExpressionInGroupBy(expression, groupByExpressions))
+        {
+            return;
+        }
+
+        switch (expression)
+        {
+            case null:
+                return;
+            case ColumnReferenceExpression columnReference:
+                result.Add(columnReference);
+                return;
+            case FunctionCall function:
+                if (IsAggregateFunction(function))
+                {
+                    return;
+                }
+                foreach (var parameter in function.Parameters)
+                {
+                    CollectColumnReferences(parameter, result, groupByExpressions);
+                }
+                CollectColumnReferencesFromOverClause(function.OverClause, result, groupByExpressions);
+                return;
+            case BinaryExpression binary:
+                CollectColumnReferences(binary.FirstExpression, result, groupByExpressions);
+                CollectColumnReferences(binary.SecondExpression, result, groupByExpressions);
+                return;
+            case ParenthesisExpression parenthesis:
+                CollectColumnReferences(parenthesis.Expression, result, groupByExpressions);
+                return;
+            case CastCall cast:
+                CollectColumnReferences(cast.Parameter, result, groupByExpressions);
+                return;
+            case ConvertCall convert:
+                CollectColumnReferences(convert.Parameter, result, groupByExpressions);
+                return;
+            case SearchedCaseExpression searchedCase:
+                foreach (var when in searchedCase.WhenClauses)
+                {
+                    CollectColumnReferencesFromBooleanExpression(when.WhenExpression, result, groupByExpressions);
+                    CollectColumnReferences(when.ThenExpression, result, groupByExpressions);
+                }
+                CollectColumnReferences(searchedCase.ElseExpression, result, groupByExpressions);
+                return;
+            case SimpleCaseExpression simpleCase:
+                CollectColumnReferences(simpleCase.InputExpression, result, groupByExpressions);
+                foreach (var when in simpleCase.WhenClauses)
+                {
+                    CollectColumnReferences(when.WhenExpression, result, groupByExpressions);
+                    CollectColumnReferences(when.ThenExpression, result, groupByExpressions);
+                }
+                CollectColumnReferences(simpleCase.ElseExpression, result, groupByExpressions);
+                return;
+            case CoalesceExpression coalesce:
+                foreach (var item in coalesce.Expressions)
+                {
+                    CollectColumnReferences(item, result, groupByExpressions);
+                }
+                return;
+            case NullIfExpression nullIf:
+                CollectColumnReferences(nullIf.FirstExpression, result, groupByExpressions);
+                CollectColumnReferences(nullIf.SecondExpression, result, groupByExpressions);
+                return;
+            case IIfCall iif:
+                CollectColumnReferencesFromBooleanExpression(iif.Predicate, result, groupByExpressions);
+                CollectColumnReferences(iif.ThenExpression, result, groupByExpressions);
+                CollectColumnReferences(iif.ElseExpression, result, groupByExpressions);
+                return;
+            case UnaryExpression unary:
+                CollectColumnReferences(unary.Expression, result, groupByExpressions);
+                return;
+            case ScalarSubquery:
+                return;
+        }
+    }
+
+    public static void CollectColumnReferencesFromBooleanExpression(
+        BooleanExpression? booleanExpression,
+        List<ColumnReferenceExpression> result,
+        List<ScalarExpression> groupByExpressions)
+    {
+        switch (booleanExpression)
+        {
+            case null:
+                return;
+            case BooleanComparisonExpression comparison:
+                CollectColumnReferences(comparison.FirstExpression, result, groupByExpressions);
+                CollectColumnReferences(comparison.SecondExpression, result, groupByExpressions);
+                return;
+            case BooleanBinaryExpression binary:
+                CollectColumnReferencesFromBooleanExpression(binary.FirstExpression, result, groupByExpressions);
+                CollectColumnReferencesFromBooleanExpression(binary.SecondExpression, result, groupByExpressions);
+                return;
+            case BooleanIsNullExpression isNull:
+                CollectColumnReferences(isNull.Expression, result, groupByExpressions);
+                return;
+            case BooleanNotExpression not:
+                CollectColumnReferencesFromBooleanExpression(not.Expression, result, groupByExpressions);
+                return;
+            case BooleanParenthesisExpression parenthesis:
+                CollectColumnReferencesFromBooleanExpression(parenthesis.Expression, result, groupByExpressions);
+                return;
+            case InPredicate inPredicate:
+                CollectColumnReferences(inPredicate.Expression, result, groupByExpressions);
+                foreach (var value in inPredicate.Values)
+                {
+                    CollectColumnReferences(value, result, groupByExpressions);
+                }
+                return;
+            case LikePredicate like:
+                CollectColumnReferences(like.FirstExpression, result, groupByExpressions);
+                CollectColumnReferences(like.SecondExpression, result, groupByExpressions);
+                return;
+            case BooleanTernaryExpression ternary:
+                CollectColumnReferences(ternary.FirstExpression, result, groupByExpressions);
+                CollectColumnReferences(ternary.SecondExpression, result, groupByExpressions);
+                CollectColumnReferences(ternary.ThirdExpression, result, groupByExpressions);
+                return;
+            case ExistsPredicate:
+                return;
+        }
+    }
+
+    public static List<ColumnReferenceExpression> FilterUngroupedColumns(
+        IEnumerable<ColumnReferenceExpression> columnReferences,
+        List<ScalarExpression> groupByExpressions)
+    {
+        var result = new List<ColumnReferenceExpression>();
+        var reportedColumns = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+        foreach (var columnReference in columnReferences)
+        {
+            if (!IsInGroupBy(columnReference, groupByExpressions) &&
+                reportedColumns.Add(GetColumnDedupKey(columnReference)))
+            {
+                result.Add(columnReference);
+            }
+        }
+
+        return result;
+    }
+
+    public static bool IsAggregateFunction(FunctionCall function) =>
+        function.OverClause is null && AggregateFunctionHelpers.IsAggregateFunction(function);
+
+    public static bool IsInGroupBy(
+        ColumnReferenceExpression columnReference,
+        List<ScalarExpression> groupByExpressions)
+    {
+        foreach (var groupByExpression in groupByExpressions)
+        {
+            if (groupByExpression is ColumnReferenceExpression groupByColumn &&
+                ColumnsMatch(columnReference, groupByColumn))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static bool IsExpressionInGroupBy(
+        ScalarExpression expression,
+        List<ScalarExpression> groupByExpressions)
+    {
+        foreach (var groupByExpression in groupByExpressions)
+        {
+            if (ExpressionsMatch(expression, groupByExpression))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static string GetColumnDisplayName(ColumnReferenceExpression columnReference)
+    {
+        var identifiers = columnReference.MultiPartIdentifier?.Identifiers;
+        return identifiers is null || identifiers.Count == 0
+            ? "?"
+            : string.Join(".", identifiers.Select(identifier => identifier.Value));
+    }
+
+    private static void CollectGroupByExpressionsFromSpecification(
+        GroupingSpecification? specification,
+        List<ScalarExpression> expressions)
+    {
+        switch (specification)
+        {
+            case null:
+                return;
+            case ExpressionGroupingSpecification expression when expression.Expression is not null:
+                expressions.Add(expression.Expression);
+                return;
+            case CompositeGroupingSpecification composite:
+                foreach (var item in composite.Items)
+                {
+                    CollectGroupByExpressionsFromSpecification(item, expressions);
+                }
+                return;
+            case CubeGroupingSpecification cube:
+                foreach (var argument in cube.Arguments)
+                {
+                    CollectGroupByExpressionsFromSpecification(argument, expressions);
+                }
+                return;
+            case RollupGroupingSpecification rollup:
+                foreach (var argument in rollup.Arguments)
+                {
+                    CollectGroupByExpressionsFromSpecification(argument, expressions);
+                }
+                return;
+            case GroupingSetsGroupingSpecification groupingSets:
+                foreach (var groupingSet in groupingSets.Sets)
+                {
+                    CollectGroupByExpressionsFromSpecification(groupingSet, expressions);
+                }
+                return;
+            case GrandTotalGroupingSpecification:
+                return;
+        }
+    }
+
+    private static void CollectColumnReferencesFromOverClause(
+        OverClause? overClause,
+        List<ColumnReferenceExpression> result,
+        List<ScalarExpression> groupByExpressions)
+    {
+        if (overClause is null)
+        {
+            return;
+        }
+
+        foreach (var partition in overClause.Partitions)
+        {
+            CollectColumnReferences(partition, result, groupByExpressions);
+        }
+
+        if (overClause.OrderByClause is not null)
+        {
+            foreach (var orderByElement in overClause.OrderByClause.OrderByElements)
+            {
+                CollectColumnReferences(orderByElement.Expression, result, groupByExpressions);
+            }
+        }
+    }
+
+    private static bool ColumnsMatch(
+        ColumnReferenceExpression left,
+        ColumnReferenceExpression right)
+    {
+        var leftIdentifiers = left.MultiPartIdentifier?.Identifiers;
+        var rightIdentifiers = right.MultiPartIdentifier?.Identifiers;
+        if (leftIdentifiers is null || rightIdentifiers is null ||
+            leftIdentifiers.Count == 0 || rightIdentifiers.Count == 0)
+        {
+            return false;
+        }
+
+        if (!string.Equals(leftIdentifiers[^1].Value, rightIdentifiers[^1].Value, StringComparison.OrdinalIgnoreCase))
+        {
+            return false;
+        }
+
+        if (leftIdentifiers.Count > 1 && rightIdentifiers.Count > 1)
+        {
+            return string.Equals(
+                leftIdentifiers[^2].Value,
+                rightIdentifiers[^2].Value,
+                StringComparison.OrdinalIgnoreCase);
+        }
+
+        return true;
+    }
+
+    private static bool ExpressionsMatch(ScalarExpression left, ScalarExpression right)
+    {
+        left = UnwrapParenthesis(left);
+        right = UnwrapParenthesis(right);
+        if (left is ColumnReferenceExpression leftColumn && right is ColumnReferenceExpression rightColumn)
+        {
+            return ColumnsMatch(leftColumn, rightColumn);
+        }
+
+        var leftText = GetNormalizedExpressionText(left);
+        var rightText = GetNormalizedExpressionText(right);
+        return leftText is not null && rightText is not null &&
+            string.Equals(leftText, rightText, StringComparison.OrdinalIgnoreCase);
+    }
+
+    private static ScalarExpression UnwrapParenthesis(ScalarExpression expression)
+    {
+        while (expression is ParenthesisExpression parenthesis)
+        {
+            expression = parenthesis.Expression;
+        }
+        return expression;
+    }
+
+    private static string? GetNormalizedExpressionText(TSqlFragment fragment)
+    {
+        var tokens = fragment.ScriptTokenStream;
+        if (tokens is null || fragment.FirstTokenIndex < 0 || fragment.LastTokenIndex < fragment.FirstTokenIndex)
+        {
+            return null;
+        }
+
+        var builder = new StringBuilder();
+        for (var index = fragment.FirstTokenIndex; index <= fragment.LastTokenIndex && index < tokens.Count; index++)
+        {
+            if (tokens[index].TokenType is TSqlTokenType.WhiteSpace or
+                TSqlTokenType.SingleLineComment or TSqlTokenType.MultilineComment)
+            {
+                continue;
+            }
+            builder.Append(NormalizeTokenText(tokens[index]));
+        }
+        return builder.ToString();
+    }
+
+    private static string NormalizeTokenText(TSqlParserToken token) =>
+        token.TokenType == TSqlTokenType.QuotedIdentifier
+            ? NormalizeQuotedIdentifier(token.Text)
+            : token.Text;
+
+    private static string NormalizeQuotedIdentifier(string text) =>
+        text.Length >= 2 && text[0] == '[' && text[^1] == ']'
+            ? text[1..^1].Replace("]]", "]", StringComparison.Ordinal)
+            : text;
+
+    private static string GetColumnDedupKey(ColumnReferenceExpression columnReference)
+    {
+        var identifiers = columnReference.MultiPartIdentifier?.Identifiers;
+        return identifiers is { Count: > 0 }
+            ? string.Join(".", identifiers.Select(identifier => identifier.Value))
+            : $"{columnReference.FirstTokenIndex}:{columnReference.LastTokenIndex}";
+    }
+}
